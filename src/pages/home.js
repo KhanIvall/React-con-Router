5b722420062fe48375f3be73a86d7e2b937f7d53
PAAWSA02LC0901 <Duoc@PAAWSA02LC0901.duoc.acad>
2025-11-04 22:03:46 -0300

    Primer commit

---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from './components/navbar';
+
+function Home() {
+    return (
+        <div>
+            <NavBar />
+            <div className="row mt-4">
+                <div className="col-md-12">
+                    <h1>dis is mai jom</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/pages/products.js
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from './components/navbar';
+
+function Products() {
+    return (
+        <div>
+            <NavBar />
+            <div className="row mt-4">
+                <div className="col-md-12">
+                    <h1>dis ar mai producs</h1>
+                    <p>lorem ipsum</p>
+
+                </div>
+
+            </div>
+        </div>
+
+    );
+}
+
+export default Products;
